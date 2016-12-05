87a07eff3388c3ed5c8477b8c45371e240c4446b
Martin Nordkvist <martin.nordkvist@zoho.com>
2016-12-05 10:05:07 +0100

    First commit in SmallTests

---
+++ SmallTestsConsole/AzureInsights.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallTestsConsole
+{
+    public class AzureInsights
+    {
+        public static void SimulatedMain()
+        {
+            BasicExample();
+
+
+
+
+            WriteLine("Nothing more to do in AzureInsights");
+
+        }
+
+        private static void BasicExample()
+        {
+
+        }
+
+
+        //Utility
+        private static string WriteLine(string text)
+        {
+            return text;
+        }
+    }
+}
