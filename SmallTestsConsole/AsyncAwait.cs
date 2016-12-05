d16b629387baebd7420cf3272e1681ec720bdd26
Martin Nordkvist <martin.nordkvist@zoho.com>
2016-12-05 13:41:20 +0100

    config file changed

---
+++ SmallTestsConsole/AsyncAwait.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmallTestsConsole
+{
+    public class AsyncAwait
+    {
+        public static  void SimulatedMain()
+        {
+            //BasicExample();
+
+            //Task task = new Task(BasicExample);
+
+            
+                          
+            WriteLine("Nothing more to do in AsyncAwait");
+
+        }
+
+        //TODO flytta in exemplet här från WinFormapplicationen
+
+
+
+        //Utility
+        private static void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+    }
+
+
+
+
+}
