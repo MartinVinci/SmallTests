2685211a11459868c621f4f2749b32ffc61dcd87
Martin Nordkvist <martin.nordkvist@zoho.com>
2016-12-05 10:49:08 +0100

    Added ReadTextfile test

---
+++ SmallTestsConsole/ReadTextfiles.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallTestsConsole
+{
+    public class ReadTextfiles
+    {
+        public static void SimulatedMain()
+        {
+
+
+
+
+            WriteLine("Nothing more to do in ReadTextfiles");
+
+        }
+        private static void ReadFiles()
+        {
+            string targetDirectory = @"C:\InFiles";
+
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+            {
+                ProcessFile(fileName);
+            }
+        }
+        public static void ProcessFile(string path)
+        {
+
+            using (var reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var fileData = line.Split(';');
+
+                    var comment = fileData[0].Trim();
+                    var amountOrdered = fileData[1].Trim();
+                    var priorityOrder = fileData[2].Trim();
+
+                    WriteLine(comment);
+                    WriteLine(amountOrdered);
+                    WriteLine(priorityOrder);
+
+
+
+                }
+            }
+        }
+
+
+
+
+
+
+        //Utility
+        private static string WriteLine(string text)
+        {
+            return text;
+        }
+
+    }
+
+}
