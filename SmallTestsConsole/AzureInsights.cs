f773f6bb3bacdde987f001c19dffacbcd289369c
Martin Nordkvist <martin.nordkvist@zoho.com>
2016-12-05 10:11:45 +0100

    Adding Insights example

--- SmallTestsConsole/AzureInsights.cs
+++ SmallTestsConsole/AzureInsights.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmallTestsConsole
@@ -18,10 +21,41 @@ namespace SmallTestsConsole
             WriteLine("Nothing more to do in AzureInsights");
 
         }
+        private static TelemetryClient telemetry;
 
         private static void BasicExample()
         {
+            TelemetryConfiguration.Active.InstrumentationKey = "b11a12a9-9eda-4da0-9a8a-0ab111834d3c";
+            //while (true)
+            //{
 
+            var dtStart = DateTime.Now;
+
+            if (telemetry == null)
+            {
+                telemetry = new TelemetryClient();
+            }
+
+            Random rand = new Random();
+            int waitingSeconds = rand.Next(2000, 5000);
+            Thread.Sleep(waitingSeconds);
+
+            var timeSpan = DateTime.Now - dtStart;
+
+            // Kolla vilken användare som skickade ordern
+            var properties = new Dictionary<string, string>();
+            properties.Add("Time Now", DateTime.UtcNow.ToString());
+
+            var metrics = new Dictionary<string, double>();
+            metrics.Add("BatmanSeeker", timeSpan.TotalSeconds);
+
+            telemetry.TrackEvent("Batman header", properties, metrics);
+
+            telemetry.Flush();
+
+            //Thread.Sleep(10000);
+
+            //}
         }
 
 
